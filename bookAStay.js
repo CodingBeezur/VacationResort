6ba33a7b74c9a4ead7f717029de93a1a257ca79f
Cesar Miranda <cesarmirandacx602@gmail.com>
2023-10-30 15:53:50 -0700

    added button to main page and bookAStay.html

---
+++ bookAStay.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+    // gathering personal details 
+    const form = document.getElementById('form');
+    form.onsubmit = getForm;
+}
+
+
+function getForm(event) {
+    event.preventDefault();
+
+    // console.log(event); testing to submit is working
+    // console.log("formSubmitted")
+
+    // get details and event details
+    let firstName = document.getElementById('firstName');
+    let customerEmail = document.getElementById('customerEmail');
+    let phoneNumber = document.getElementById('phoneNumber');
+    let checkInDate = document.getElementById('checkInDates')
+    let numberOfNights = document.getElementById('numberOfNights'); 
+
+
+    // get discount details 
+    const militaryDiscountRadio = document.getElementById('militaryDiscount')
+    const seniorDiscountRadio = docment.getElementById('seniorDiscount')
+    const noDiscountRadio = document.getElementById('noDiscount');
+
+    // get bedSize details
+    let queenSize = document.getElementById('queenSizedBed');
+    let kingSize = document.getElementById('kingSizedBed');
+    let suiteSize = document.getElementById('suitSizeBed');
+
+}
+
